4de8a9d3f3713c5c2e06dc7eb5f582f5525f091d
Brian Cloutier <github@briancloutier.com>
2018-06-28 17:16:47 -0700

    Remove tests from this PR

--- src/test/regress/sql/failure_setup.sql
+++
@@ -1,5 +0,0 @@
-SELECT citus.mitmproxy('conn.allow()');
-
--- add the workers
-SELECT master_add_node('localhost', :worker_1_port);  -- the second worker
-SELECT master_add_node('localhost', :worker_2_port + 2);  -- the first worker, behind a mitmproxy
